f230afdaa58189a72fb55de49a79a506c33388e3
agc123 <agc123@yeah.net>
2017-02-25 17:58:53 -0600

    server with pg

--- server.js
+++ server.js
@@ -2,14 +2,33 @@ const db = require('./db');
 const fs = require('fs');
 const http = require('http');
 const ndjson = require('ndjson');
+const pg = require('pg');
 
 module.exports = function({ cwd = process.cwd() } = {}){
 	var apiRequest = /api/;
+	
+
+	const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+	const client = new pg.Client(connectionString);
+	
 
 	var server = http.createServer(function(req, res){
 
 		if(apiRequest.test(req.url)) {
-			db().pipe(ndjson.serialize()).pipe(res);
+			//db().pipe(ndjson.serialize()).pipe(res);
+			client.connect(connectionString,function(err, client, done)  {
+				if (err) {
+					console.log(err);
+					throw err;
+				}
+				const query = client.query('Select * from todos');
+				query.on('row', (row) => {
+					console.log(row);
+			      	res.write(row);
+			    });
+			})
+	
+			
 		} else {
 			var url = req.url;
 			if(url === '/')
